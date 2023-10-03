4444ba65e3e4ae3a42131adbbf203ab8077ee68c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-03 12:59:44 -0300

    login

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Conecte-se ao banco de dados MongoDB
+mongoose.connect('mongodb://localhost/cadastro', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Defina um modelo para os dados
+const Cadastro = mongoose.model('Cadastro', {
+    nomeCompleto: String,
+    email: String,
+    endereco: String,
+    bairro: String,
+    complemento: String,
+    numero: String,
+    cep: String,
+    uf: String
+});
+
+// Rota para exibir o formulário
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Rota para processar o envio do formulário
+app.post('/cadastrar', (req, res) => {
+    const {
+         nomeCompleto, 
+        email,
+        endereco,
+        bairro,
+        complemento,
+        numero,
+        cep,
+        uf
+    } = req.body;
+
+    // Crie um novo documento no banco de dados
+    const novoCadastro = new Cadastro({ nomeCompleto, email,endereco,bairro,complemento,numero,cep,uf });
+
+    // Salve o documento
+    novoCadastro.save(err => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro ao cadastrar.');
+        } else {
+            res.redirect('/sucesso.html'); // Página de sucesso após o cadastro
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
